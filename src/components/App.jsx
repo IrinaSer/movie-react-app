3ae1af55ac12d9286fba118376a5aba6ef07fe12
IrinSer <serebryakova.irin.i@gmail.com>
2020-06-01 21:29:41 +0300

    add get movie by sort

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,18 +14,26 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log("componentDidMount");
+    this.getMovies();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.sort_by !== this.state.sort_by) {
+      this.getMovies();
+    }
+  }
+
+  getMovies = () => {
     fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
         this.setState({
           movies: data.results,
         });
       });
-  }
+  };
 
   removeMovie = (movie) => {
     const updateMovies = this.state.movies.filter(function (item) {
--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 
-const MovieTabs = (props) => {
-  const { sort_by, updateSortBy } = props;
-  const handleClick = (value) => () => {
-    updateSortBy(value);
-  };
-  const getClass = (value) => {
-    return `nav-link ${sort_by === value ? "active" : ""}`;
-  };
-  return (
-    <ul className="tabs nav nav-pills">
-      <li className={getClass("popularity.desc")} onClick={handleClick("popularity.desc")}>
-        <div className="nav-link">Popularity desc</div>
-      </li>
-      <li className={getClass("revenue.desc")} onClick={handleClick("revenue.desc")}>
-        <div className="nav-link">Revenue desc</div>
-      </li>
-      <li className={getClass("vote_average.desc")} onClick={handleClick("vote_average.desc")}>
-        <div className="nav-link">Vote average desc</div>
-      </li>
-    </ul>
-  );
-};
+class MovieTabs extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.sort_by !== this.sort_by) {
+      return true;
+    }
+    return false;
+  }
+  render() {
+    const { sort_by, updateSortBy } = this.props;
+    const handleClick = (value) => () => {
+      updateSortBy(value);
+    };
+    const getClass = (value) => {
+      return `nav-link ${sort_by === value ? "active" : ""}`;
+    };
+    return (
+      <ul className="tabs nav nav-pills">
+        <li className={getClass("popularity.desc")} onClick={handleClick("popularity.desc")}>
+          <div className="nav-link">Popularity desc</div>
+        </li>
+        <li className={getClass("revenue.desc")} onClick={handleClick("revenue.desc")}>
+          <div className="nav-link">Revenue desc</div>
+        </li>
+        <li className={getClass("vote_average.desc")} onClick={handleClick("vote_average.desc")}>
+          <div className="nav-link">Vote average desc</div>
+        </li>
+      </ul>
+    );
+  }
+}
 
 export default MovieTabs;
